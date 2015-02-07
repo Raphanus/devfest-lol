a45820e5e992c720847ce0c87558dba19f4beb3a
eodnhoj <michael@modernliteracy.org>
2015-02-06 22:09:26 -0500

    Wheeee

--- data.py
+++ data.py
@@ -21,6 +21,22 @@ def ranked_stats(summoner, region="NA", season="SEASON2014"):
     js = get_ranked_stats(region, summoner, season)
     return js 
 
+def get_challengers(region="NA", queuetype="RANKED_SOLO_5x5"):
+    """
+    queuetype can be RANKED_TEAM_5x5 and RANKED_TEAM_3x3 too.
+    """
+    js = get_challenger_league_tiers(region,queuetype)
+    return js
+
+def get_all_challengers(region="NA"):
+    queues = ["RANKED_SOLO_5X5",
+            "RANKED_TEAM_5X5"]
+    l_js = []
+    for q in queues:
+        l_js.append(get_challengers(region,q))
+    pprint(l_js)
+
 if __name__ == "__main__":
     #pprint(rec_gms_to_kda('lexwraith'))
-    pprint(ranked_stats("lexwraith"))
+    #pprint(ranked_stats("lexwraith"))
+    g =  get_all_challengers()
